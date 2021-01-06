307d0be4a3ca609b5546de62e99384d36d0bf7aa
Arianna Basha <ariannagbasha@gmail.com>
2021-01-06 09:25:22 -0600

    fixing the models

--- imdb_app/views.py
+++ imdb_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+# Everytime you submit a review, you will be editing the rating for the movie of that model
 def index(request):
     pass
+
+def review_submission(request):
+    # Have a form to submit 
+    # Get the specific movie that you are submitting
+    #
