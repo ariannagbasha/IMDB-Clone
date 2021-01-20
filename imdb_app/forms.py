447ac43426fcfb8129ca0895e73bdf1f9c18b728
Arianna Basha <ariannagbasha@gmail.com>
2021-01-20 09:16:00 -0600

    merged styling stuff

--- imdb_app/forms.py
+++ imdb_app/forms.py
@@ -8,7 +8,7 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.Form):
     username = forms.CharField(max_length=150,)
     first_name = forms.CharField(max_length=350, required=True)
     last_name = forms.CharField(max_length=350, required=True)
